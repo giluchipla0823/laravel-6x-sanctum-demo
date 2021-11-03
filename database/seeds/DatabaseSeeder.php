9e274b874ab822e385710f14b6dd51470e62f578
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2021-11-03 02:44:31 +0100

    Implementando sanctum

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        // Truncate
+        User::truncate();
+
+        factory(User::class, 50)->create();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
